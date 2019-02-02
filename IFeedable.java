7707fd26909f722c601c7d7a51d3f732043ec0fb
Abel Shinabery <aps81@txstate.edu>
2019-02-02 12:13:37 -0600

    Added documentation to interface files

--- IFeedable.java
+++ IFeedable.java
@@ -1,3 +1,15 @@
+/**
+*	
+* We moved the eat function to it's own interface because 
+* some employees do not use the eat method originally 
+* included. (i.e. Robot) This follows the Interface
+* Segregation principle by spreading out one big interface
+* into multiple interfaces.
+*
+* @author Abel
+*
+*/
+
 package threesolid;
 
 interface IFeedable {
--- IWorkable.java
+++ IWorkable.java
@@ -1,3 +1,15 @@
+/**
+*	
+* We moved the work function to it's own interface because 
+* some employees do not use the eat method originally 
+* included. (i.e. Robot) This follows the Interface
+* Segregation principle by spreading out one big interface
+* into multiple interfaces.
+*
+* @author Abel
+*
+*/
+
 package threesolid;
 
 interface IWorkable {
