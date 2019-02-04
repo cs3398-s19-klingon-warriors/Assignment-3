cbd74c041568be663079735f1a8189afb39b5938
rkelmo <rkelmo@users.noreply.github.com>
2019-02-04 13:36:20 -0600

    Addition to interface principle explanation

--- ThreeSolidPrinciples/src/IWorkable.java
+++ ThreeSolidPrinciples/src/IWorkable.java
@@ -4,7 +4,8 @@
 * some employees do not use the eat method originally 
 * included (i.e. Robot). This follows the Interface
 * Segregation principle by spreading out one big interface
-* into multiple interfaces.
+* into multiple interfaces, so that classes implementing
+* each interface do not need to implement any irrelevant or unused functions.
 *
 * @author Abel
 *
