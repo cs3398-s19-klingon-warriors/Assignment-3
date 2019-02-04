d89f5a1eefe3ab717d0bdfe07d99cd0c081dfae9
rkelmo <rkelmo@users.noreply.github.com>
2019-02-04 13:16:36 -0600

    Finicky

--- ThreeSolidPrinciples/src/IWorkable.java
+++ ThreeSolidPrinciples/src/IWorkable.java
@@ -1,8 +1,8 @@
 /**
 *	
-* We moved the work function to it's own interface because 
+* We moved the work function to its own interface because 
 * some employees do not use the eat method originally 
-* included. (i.e. Robot) This follows the Interface
+* included (i.e. Robot). This follows the Interface
 * Segregation principle by spreading out one big interface
 * into multiple interfaces.
 *
