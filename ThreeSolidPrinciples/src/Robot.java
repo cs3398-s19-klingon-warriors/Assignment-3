573b5b709d15522b049ba7e86a25fe0e447a1001
Talon <tks49@txstate.edu>
2019-02-04 18:38:41 -0600

    Included name in file
    Also referenced to workingdraft.txt but can change when/if we adjust the location or name.

--- ThreeSolidPrinciples/src/Robot.java
+++ ThreeSolidPrinciples/src/Robot.java
@@ -1,4 +1,11 @@
+/* 
+   Talon Sjostrom
+   
+   Three Principles explanation found in Assignment-3/workingdraft.txt
+*/
+
 package threesolid;
+
 
 class Robot implements IWorkable{
 	public void work() {
