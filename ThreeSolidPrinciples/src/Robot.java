311e741c4b81f5a73428d059e2262fbc72ac93ed
rkelmo <rkelmo@users.noreply.github.com>
2019-02-04 21:36:10 -0600

    undo package move, sorry!

--- ThreeSolidPrinciples/src/Robot.java
+++ ThreeSolidPrinciples/src/Robot.java
@@ -1,11 +1,10 @@
-package threesolid;
-
 /* 
    Talon Sjostrom
    
    Three Principles explanation found in Assignment-3/A3_Klingons.txt
 */
 
+package threesolid;
 
 class Robot implements IWorkable{
 	public void work() {
