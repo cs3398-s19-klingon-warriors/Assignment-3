f917484f576e33f93095e257473c362f7355b0b4
Abel Shinabery <aps81@txstate.edu>
2019-02-14 13:59:32 -0600

    Added all possible classes and method calls to main, commented out for future use

--- ThreeSolidPrinciples/src/ThreeSolidMain.java
+++ ThreeSolidPrinciples/src/ThreeSolidMain.java
@@ -11,9 +11,24 @@ import java.io.*;
 
 public class ThreeSolidMain
 {
-
+    
+  /*
+  ==================
+  Classes
+  ==================
+  */
+    
   public static Manager tsManager = new Manager();
-
+  //public tsProjectManager = new ProjectManager();
+  //public tsProductManager = new ProductManager();
+    
+  
+  //public tsBaseWorker = new BaseWorker(); (Possibly abstract?)
+  //public tsRobot = new Robot();
+  //public tsWorker = new Worker();
+  //public tsSuperWorker = new SuperWorker();
+  //public tsTempWorker = new TempWorker();
+    
   // The entry main() method
   public static void main(String[] args)
   {
@@ -21,6 +36,76 @@ public class ThreeSolidMain
     try
     {
       System.out.format("Starting ... \n");
+      /*
+      ===============
+      Manager
+      ===============
+      */    
+      //tsManager.setWorker(tsWorker);
+      //tsManager.manage();
+        
+      /*
+      ===============
+      Project Manager
+      ===============
+      */
+      //tsProjectManager.scheduleWork();
+      //tsProjectManager.setWorker(tsWorker);
+      //tsProjectManager.manage();
+      
+      /*
+      ===============
+      Product Manager
+      ===============
+      */
+      //tsProductManager.defineProduct();
+      //tsProductManager.setWorker(tsWorker);
+      //tsProductManager.manage();
+        
+      /*
+      ===============================
+      Base Worker (Might be abstract)
+      ===============================
+      */
+      //tsBaseWorker.work();
+      //tsBaseWorker.eat();
+      //tsBaseWorker.sick();
+        
+      /*
+      ===============
+      Robot
+      ===============
+      */
+      //tsRobot.work();
+      //tsRobot.reboot();
+    
+      /*
+      ===============
+      Worker
+      ===============
+      */
+      //tsWorker.work();
+      //tsWorker.eat();
+      //tsWorker.sick();
+        
+      /*
+      ===============
+      Super Worker
+      ===============
+      */
+      //tsSuperWorker.work();
+      //tsSuperWorker.eat();
+      //tsSuperWorker.sick();
+    
+      /*
+      ===============
+      Temp Worker
+      ===============
+      */
+      //tsTempWorker.work();
+      //tsTempWorker.eat();
+      //tsTempWorker.sick();
+      
     }
     catch (Exception main_except)
     {
