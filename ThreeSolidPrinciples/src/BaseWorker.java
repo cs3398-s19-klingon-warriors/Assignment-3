053d3fcd5862ff0a90db1ad9c761406ec7ff0dab
serena-marie <serena.gutierrez0@gmail.com>
2019-02-16 12:13:20 -0600

    Upload New File: BaseWorker.java

---
+++ ThreeSolidPrinciples/src/BaseWorker.java
@@ -0,0 +1,11 @@
+/*
+Author: Serena Gutierrez
+*/
+
+package threesolid;
+
+public class BaseWorker(){
+    public void work(){
+        System.out.println("Default Work: Werk werk werk werk");
+    }
+}
