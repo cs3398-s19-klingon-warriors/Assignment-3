869f1d37415abdc1e9b51d66da89bcd883be5141
rkelmo <rkelmo@users.noreply.github.com>
2019-02-18 16:08:36 -0600

    Added Product and Project Manager files via upload

---
+++ ThreeSolidPrinciples/src/ProductManager.java
@@ -0,0 +1,22 @@
+/*
+ * Product Manager
+ * extends Manager class
+ * Rachel Kelmenson
+ */
+ 
+
+package threesolid ;
+
+public class ProductManager extends Manager
+{
+	public void ProductManager()
+	{
+		System.out.format("Creating a ProductManager ... \n");
+	}
+
+	public void defineProduct()
+	{
+		System.out.format("The product manager is defining a product...let's hope Apple hasn't thought of this yet ... \n");
+	}
+}
+
---
+++ ThreeSolidPrinciples/src/ProjectManager.java
@@ -0,0 +1,21 @@
+/*
+ * Project Manager
+ * extends Manager class
+ * Rachel Kelmenson
+ */
+ 
+
+package threesolid ;
+
+public class ProjectManager extends Manager
+{
+	public void ProjectManager()
+	{
+		System.out.format("Creating a ProjectManager ... \n");
+	}
+
+	public void scheduleWork()
+	{
+		System.out.format("The project manager is scheduling work...don't be late! ... \n");
+	}
+}
