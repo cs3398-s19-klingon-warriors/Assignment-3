601a1465ae4629f97bfda5ef6a8dbce8c49d6511
Talon S <32073940+TalonAsInClaw@users.noreply.github.com>
2019-02-18 14:22:36 -0600

    Update Robot
    Updating Robot.java to use default work().

--- ThreeSolidPrinciples/src/Robot.java
+++ ThreeSolidPrinciples/src/Robot.java
@@ -7,11 +7,6 @@
 package threesolid;
 
 class Robot extends BaseWorker implements IReboot{
-	public void work() {
-		System.out.println("Robot: \n\tOf course they send the robot to do janitorial work.");
-		System.out.println("\tIt's like they think I don't have a highly sophisticated");
-		System.out.println("\temotional response system in place.\n");
-	}
 	public void reboot() {
 		System.out.println("Robot: \n\tRebooting... and done.\n");
 	}
