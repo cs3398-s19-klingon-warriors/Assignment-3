ccd4f70825f5e8909cec42bb5150c5a84dcc3993
serena-marie <serena.gutierrez0@gmail.com>
2019-04-23 18:25:01 -0500

    gradle build successful

--- ThreeSolidPrinciples/src/test/java/threesolid/TestWorker.java
+++ ThreeSolidPrinciples/src/test/java/threesolid/TestWorker.java
@@ -2,19 +2,23 @@
 
 package threesolid;
 
-//import static org.junit.jupiter.api.Assertions.*;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+//import static org.junit.Assert.*;
+//import org.junit.Before;
+//import org.junit.Test;
 
 import org.junit.jupiter.api.*;
 
-public class TestWorker
-{
-private Worker w = new Worker();
-@Test
-@DisplayName("Work Method Test")
-public void testWorkMethod()
-{
-assertEquals(w.new_work(),"I'm working already!","Mismatch between test text and method text");
-}
+public class TestWorker{
+    private Worker w = new Worker();
+
+    @Test
+    @DisplayName("Work Method Test")
+
+    public void testWorkMethod(){
+        assertEquals(w.new_work(),"I'm working already!","Mismatch between test text and method text");
+    }
 }
