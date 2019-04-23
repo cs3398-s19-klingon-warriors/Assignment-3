c0601e17efdaa3747659cdad36599392ea03c67d
rkelmo <kelmette88@gmail.com>
2019-04-23 18:29:52 -0500

    up to date with master

--- ThreeSolidPrinciples/src/test/java/threesolid/TestWorker.java
+++ ThreeSolidPrinciples/src/test/java/threesolid/TestWorker.java
@@ -5,26 +5,13 @@ package threesolid;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-<<<<<<< HEAD
-=======
 
 //import static org.junit.Assert.*;
 //import org.junit.Before;
 //import org.junit.Test;
->>>>>>> ccd4f70825f5e8909cec42bb5150c5a84dcc3993
 
+import org.junit.jupiter.api.*;
 
-<<<<<<< HEAD
-public class TestWorker
-{
-private Worker w = new Worker();
-@Test
-@DisplayName("Work Method Test")
-public void testWorkMethod()
-{
-assertEquals(w.new_work(),6,"Mismatch between test text and method text");
-}
-=======
 public class TestWorker{
     private Worker w = new Worker();
 
@@ -34,5 +21,4 @@ public class TestWorker{
     public void testWorkMethod(){
         assertEquals(w.new_work(),"I'm working already!","Mismatch between test text and method text");
     }
->>>>>>> ccd4f70825f5e8909cec42bb5150c5a84dcc3993
 }
