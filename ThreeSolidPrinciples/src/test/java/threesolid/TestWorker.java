74960df10ade84e3b87fc659366993994b13d7a6
rkelmo <kelmette88@gmail.com>
2019-04-23 17:34:50 -0500

    add test files and dir

---
+++ ThreeSolidPrinciples/src/test/java/threesolid/TestWorker.java
@@ -0,0 +1,17 @@
+// copy from A17
+
+package threesolid;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class TestWorker
+{
+private Worker w = new Worker();
+@Test
+@DisplayName("Work Method Test") public void testWorkMethod()
+{
+assertEquals(w.work(),"I'm working already!",”Mismatch between test text and method text”);
+}
+}
