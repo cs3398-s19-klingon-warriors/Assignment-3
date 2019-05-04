c9234f3aa81496dac5392c378cc98dcde0c9b0b3
rkelmo <kelmette88@gmail.com>
2019-05-03 21:06:22 -0500

    cleaned and clarified authorship

--- ThreeSolidPrinciples/src/test/java/threesolid/TestWorker.java
+++ ThreeSolidPrinciples/src/test/java/threesolid/TestWorker.java
@@ -16,14 +16,14 @@ public class TestWorker{
     private Worker w = new Worker();
 
     @Test
-    @DisplayName("New Work Method Test")
+    @DisplayName("New Work Method Test") // rk modify
     public void testWorkMethod(){
         assertEquals(w.new_work(),"Ready for new work","Mismatch between test text and method text");
     }
 
     @Test
     @DisplayName("Break Time Test")
-    public void checkBreakTime()
+    public void checkBreakTime() // rk add
     {
 	int current_time = 6 ;
         assertNotSame(w.break_time(),current_time,"Problem: worker needs to take a break");
@@ -31,7 +31,7 @@ public class TestWorker{
 
       @Test
     @DisplayName("All Good Test")
-    public void testAllGood()
+    public void testAllGood() // rk add
     {
         assertTrue(w.all_good(),"Oh no! We are not all good");
     }
